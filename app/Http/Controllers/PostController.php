dc2d61a97f6c649270a6e4aeb50d953ce84a1575
Carolina Ruiz <ing.acrd@gmail.com>
2024-05-14 13:58:08 -0400

    MySql conf, migrations and crud operations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,11 +14,13 @@ class PostController extends Controller
     {
         //get all post from database
 
-        $name = 'Alfred';
-        $age = 32;
-        $posts = ['post 1', 'post 2', 'post 3'];
+        // $name = 'Alfred';
+        // $age = 32;
+        // $posts = ['post 1', 'post 2', 'post 3'];
+        $posts = Post::all();
+        // return view('posts.index', ['username' => $name, 'age' => $age, 'posts' => $posts]);
+        return view('posts.index', ['posts' => $posts]);
 
-        return view('posts.index', ['username' => $name, 'age' => $age, 'posts' => $posts]);
     }
 
     /**
@@ -34,23 +36,39 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request);
+        $validated = $request->validate([
+            //'title' => 'required|max:255',
+            'title' => ['required', 'max:255', 'min:5'],
+            //'content' => 'required',
+            'content' => ['required', 'min:10'],
+        ]);
+
+        // Post::create([
+        //     'title' => $request->title,
+        //     'content' => $request->content,
+        // ]);
+        Post::create($validated);
+        //return redirect()->route('posts.index');
+        return to_route('posts.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show()
+    //public function show($id)
+    public function show(Post $post)
     {
-        return view('posts.show');
+        //$post = Post::findOrFail($id);
+        return view('posts.show', ['post' => $post]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit()
+    public function edit(Post $post)
     {
-        return view('posts.edit');
+        return view('posts.edit', ['post' => $post]);
     }
 
     /**
@@ -58,7 +76,14 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $validated = $request->validate([
+            'title' => ['required', 'max:255', 'min:5'],
+            'content' => ['required', 'min:10'],
+        ]);
+
+        $post->update($validated);
+        return to_route('posts.index');
+
     }
 
     /**
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,16 @@
 <x-layout>
-    <h1>Posts show</h1>
+    <section class="mt-4">
+        <div class="flex justify-end">
+            <a href="{{ route('posts.edit', $post->id) }}"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Edit
+            </a>
+        </div>
+    </section>
+    <div class="max-w-6x1 mx-auto sm:px-6 lg:px-8 mt-8 bg-slate-50 rounded-lg ">
+        <h1 class="text-3xl text-indigo-800 font-semibold">{{ $post->title }}</h1>
+        <main class="max-w-6xl mx-auto mt-6 lg:mt:20 space-y-6">
+            <p class="text-gray-700 p-4 mb-4">{{ $post->content }}</p>
+        </main>
+    </div>
 </x-layout>
