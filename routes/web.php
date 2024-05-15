6ddaa6f4afcb945c209c87b446df245cbff7b89f
Carolina Ruiz <ing.acrd@gmail.com>
2024-05-15 09:38:43 -0400

    register, login, logout

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
+use App\Http\Controllers\LoginUserController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::view('/', 'welcome');
-
-
 Route::resource('/posts', PostController::class);
+Route::get('/register', [RegisterUserController::class, 'register'])->name('register');
+Route::post('/register', [RegisterUserController::class, 'store'])->name('register.store');
+Route::get('/login', [LoginUserController::class, 'login'])->name('login');
+Route::post('/login', [LoginUserController::class, 'store'])->name('login.store');
+Route::post('/logout', [LoginUserController::class, 'logout'])->name('logout');
