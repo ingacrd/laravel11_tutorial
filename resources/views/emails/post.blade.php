d508a1d09f54080420d2cc867b8c4febe9797154
Carolina Ruiz <ing.acrd@gmail.com>
2024-05-21 10:56:44 -0400

    send email when post created

---
+++ resources/views/emails/post.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Thank you for the post {{$data['name']}}
+
+Your post title is: {{$data['title']}}
+
+{{--<x-mail::button :url="''">--}}
+{{--Button Text--}}
+{{--</x-mail::button>--}}
+
+Thanks, Laravel Tutorial
+
+</x-mail::message>
